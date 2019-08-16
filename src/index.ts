d85ad6f522768026ff55e250739603a741610b76
Pascal Iske <info@pascal-iske.de>
2019-08-16 14:03:49 +0200

    chore(eslint): integrate typescript-eslint into config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import rules from './rules'
+
 export = {
-    rules: {},
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+    ],
+    rules,
 }
